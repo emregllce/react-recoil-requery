8d518ef7323262f6996c64dac171f6e039b5994e
emregllce <emregllce@gmail.com>
2023-01-14 17:21:11 +0300

    Homecard clean

--- src/HomeCard.js
+++ src/HomeCard.js
@@ -1,23 +1,12 @@
 import * as React from "react";
-// import Card from "@mui/material/Card";
-// import CardActions from "@mui/material/CardActions";
-// import CardContent from "@mui/material/CardContent";
-// import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import {
-  AppBar,
-  Badge,
   Box,
   Card,
   CardActions,
   CardContent,
   CardMedia,
-  InputBase,
-  Menu,
-  MenuItem,
   styled,
-  Toolbar,
   Typography
 } from "@mui/material";
 
